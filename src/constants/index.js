aac7bf52f8b06fba0d7c85c58f6f0615a77754fd
YunusEmreUslu <usluyunusemre00@gmail.com>
2023-10-08 13:06:44 +0300

    changed some text

--- src/constants/index.js
+++ src/constants/index.js
@@ -47,7 +47,7 @@ export const feedback = [
   {
     id: "feedback-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+      "Nullam interdum erat eu sapien suscipit, id pellentesque justo sodales. Mauris sodales lorem laoreet lacinia dapibus.",
     name: "Herman Jensen",
     title: "Founder & Leader",
     img: people01,
@@ -55,7 +55,7 @@ export const feedback = [
   {
     id: "feedback-2",
     content:
-      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+      "Integer est ligula, dictum vel tristique id, porta eget tellus. Quisque facilisis nec lacus a consequat.",
     name: "Steve Mark",
     title: "Founder & Leader",
     img: people02,
@@ -63,7 +63,7 @@ export const feedback = [
   {
     id: "feedback-3",
     content:
-      "It is usually people in the money business, finance, and international trade that are really rich.",
+      "Aliquam porttitor urna quis tellus porta consectetur. Nam iaculis suscipit tempor. Fusce aliquam massa et lobortis sodales.",
     name: "Kenn Gallagher",
     title: "Founder & Leader",
     img: people03,
